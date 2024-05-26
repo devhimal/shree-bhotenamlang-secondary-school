4d6d6601ba0fd36cd792c4a391acb0ba2a020b1b
devhimal <himalhobbs.364@gmail.com>
2024-05-26 08:17:10 +0545

    fix: event card bg removed and border added.

--- src/app/components/ui/BentroGrid.tsx
+++ src/app/components/ui/BentroGrid.tsx
@@ -11,7 +11,7 @@ export const BentoGrid = ({
   return (
     <div
       className={cn(
-        "grid md:auto-rows-[18rem] h-full   grid-cols-1 md:grid-cols-3 gap-4 max-w-7xl mx-auto ",
+        "grid md:auto-rows-[18rem] h-full grid-cols-1 md:grid-cols-3 gap-4 max-w-7xl mx-auto ",
         className
       )}
     >
@@ -37,7 +37,7 @@ export const BentoGridItem = ({
   return (
     <div
       className={cn(
-        "row-span-1 rounded-xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col",
+        "row-span-1   rounded-xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 text-black  bg-white border border-green-700 justify-between flex flex-col",
         className
       )}
     >
@@ -51,13 +51,9 @@ export const BentoGridItem = ({
         />
       </div>
       <div className="group-hover/bento:translate-x-2 transition duration-200">
-        <p className="text-white py-2 pt-4 font-sm">{icon}</p>
-        <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
-          {title}
-        </div>
-        <div className="font-sans font-normal text-neutral-600 text-xs dark:text-neutral-300">
-          {description}
-        </div>
+        <p className=" py-2 pt-4 font-sm">{icon}</p>
+        <div className="font-sans font-bold   mb-2 mt-2">{title}</div>
+        <div className="font-sans font-normal text-xs ">{description}</div>
       </div>
     </div>
   )
