6adef5650ec1b95ed8722f1ba59d0226e5027baa
devhimal <himalhobbs.364@gmail.com>
2024-06-01 08:01:04 +0545

    fix: updated the tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,8 +7,6 @@ const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./src/components/*.{ts,js,tsx,jsx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
